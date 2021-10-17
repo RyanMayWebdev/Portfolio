5142bf58594a381f8336da0752819b0f1beee773
Ryan May <ryan777may@gmail.com>
2021-10-17 19:22:39 -0400

    Completed scroll tiles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const portfolio = {};
 portfolio.dropdown = document.querySelector('#menuDrop');
 portfolio.navMenu = document.querySelector('#navDropdown')
+portfolio.navLinks = document.querySelectorAll('.navLink');
+portfolio.scrollWrapper = document.querySelector('.scrollWrapper');
+
 
 portfolio.nav = () => {
     portfolio.dropdown.addEventListener('click', function () {
-        console.log('click');
         portfolio.navMenu.classList.toggle('revealNav');
     });
+
+    portfolio.navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            portfolio.scrollWrapper.scrollTo(0,0 );
+        })
+    })
 };
+
 
 portfolio.init = () => {
     portfolio.nav();
